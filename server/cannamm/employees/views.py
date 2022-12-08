fa8d07ab998c98fbc140246f3ac1cc401fba8f56
jamestha3d <jamesikediashi@gmail.com>
2022-12-08 03:05:18 -0500

    update success

--- server/cannamm/employees/views.py
+++ server/cannamm/employees/views.py
@@ -34,7 +34,6 @@ def employeeDetail(request, pk):
 @api_view(['POST'])
 def employeeCreate(request):
     data = request.data
-
     #using serializer to save city
     cityData = {
         "name": data["city"],
@@ -68,11 +67,43 @@ def employeeCreate(request):
 @api_view(['POST'])
 def employeeUpdate(request, pk):
     employee = Employee.objects.get(id=pk)
+    data = request.data 
+
+    cityData = {
+        "name": data["city"],
+        "state": data["state"],
+        "zipcode": data["zipcode"]
+    }
+    city = City.objects.filter(name=cityData["name"], state=cityData["state"], zipcode=cityData["zipcode"])
+
+    if not city:
+        city_serializer = CitySerializer(data=cityData)
+        if city_serializer.is_valid():
+            city_serializer.save()
+            city_id = City.objects.get(name=cityData["name"], state=cityData["state"], zipcode=cityData["zipcode"]).id
+    else:
+        city_id = city[0].id
+
+    city = City.objects.get(pk=city_id)
+
+    employee.givenName = data["givenName"]
+    employee.gender = data["gender"]
+    employee.birthday = data["birthday"]
+    employee.title = data["title"]
+    employee.middleInitial = data["middleInitial"]
+    employee.surname = data["surname"]
+    employee.city = city
+    employee.save()
+
+   
     #edit here
-    serializer = EmployeeSerializer(instance=employee, data=request.data)
+    """ serializer = EmployeeSerializer(instance=employee, data=request.data)
     if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+        serializer.save() """
+
+
+    
+    return Response("update success")
 
 @api_view(['DELETE'])
 def employeeDelete(request, pk):
